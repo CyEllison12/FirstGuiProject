9427918bcb73b7bb62daf59a3db339b2c7db651a
Cy Ellison <dragonz2626@gmail.com>
2018-11-20 12:28:04 -0700

    added duck, and view. added code to open up the window and make it.

---
+++ src/gui/model/Duck.java
@@ -0,0 +1,18 @@
+package gui.model;
+
+public class Duck
+{
+	private String sound;
+	
+	public Duck()
+	{
+		this.sound = "Quack Quack";
+	}
+	
+	public String getSound()
+	{
+		return sound;
+	}
+	
+	
+}
